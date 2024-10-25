f88a614485506186ac5fe1752227c51177745634
CoderAnjalii <116449521+CoderAnjalii@users.noreply.github.com>
2024-10-25 22:40:32 +0530

    $

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
-const PORT = process.env.PORT ||  3000
+const PORT = process.env.PORT ||  3004
 server.listen(PORT,()=>{
     dbConnect();
     console.log(`Working at port ${PORT}`);
